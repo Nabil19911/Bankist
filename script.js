62424e41e3a2e74ed57c80c921103222744c0603
Nabil <nabilshaahani@gmail.com>
2021-04-25 22:25:57 +0400

    updated

--- script.js
+++ script.js
@@ -61,6 +61,148 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+// Update the HTML DOM
+const displayMovements = movements => {
+  containerMovements.innerHTML = '';
+  movements.forEach((mov, i) => {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+          <div class="movements__row">
+            <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+            <div class="movements__date">3 days ago</div>
+            <div class="movements__value">${mov}€</div>
+          </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+// Dispaly the total amount
+const calcDispalyBalance = acc => {
+  acc.balance = acc.movements.reduce((acc, cur) => acc + cur, 0);
+  labelBalance.textContent = `${acc.balance}€`;
+};
+
+// Update the user UI
+const calcDisplaySummary = acc => {
+  const incomes = acc.movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const outgoing = acc.movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(outgoing)}€`;
+
+  const interest = acc.movements
+    .filter(mov => mov > 0)
+    .map(mov => (mov * acc.interestRate) / 100)
+    .filter(mov => mov >= 1)
+    .reduce((acc, mov) => acc + mov, 0);
+
+  labelSumInterest.textContent = `${interest}€`;
+};
+
+// Create username with first letters in the names
+const createUserName = accs => {
+  accs.forEach(acc => {
+    acc['username'] = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(user => user[0])
+      .join('');
+  });
+};
+createUserName(accounts);
+
+// Update the UI
+const updateUI = acc => {
+  displayMovements(acc.movements);
+  calcDispalyBalance(acc);
+  calcDisplaySummary(acc);
+};
+
+// LOGIN EVENT
+let currentAccount;
+btnLogin.addEventListener('click', e => {
+  e.preventDefault();
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  // console.log(currentAccount);
+  if (currentAccount?.pin === +inputLoginPin.value) {
+    // Update ui
+    updateUI(currentAccount);
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+    containerApp.style.opacity = 100;
+  } else {
+    labelWelcome.textContent = `Log in to get started`;
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+    containerApp.style.opacity = 0;
+  }
+});
+
+// TRANSFER EVENT
+btnTransfer.addEventListener('click', e => {
+  e.preventDefault();
+  // Get the user input amount value
+  const amount = +inputTransferAmount.value;
+
+  // Find the account object that holds userinput value username
+  const transferTo = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+
+  // Reset the user input
+  inputTransferTo.value = inputTransferAmount.value = '';
+  inputTransferAmount.blur();
+
+  // Check the validation
+  if (
+    transferTo &&
+    amount > 0 &&
+    currentAccount.balance >= amount &&
+    transferTo?.username !== currentAccount.value
+  ) {
+    // Push to movement array
+    currentAccount.movements.push(-amount);
+    transferTo.movements.push(amount);
+
+    // Show little delay in updating UI
+    setTimeout(() => {
+      updateUI(currentAccount);
+    }, 1000);
+  }
+});
+// LOAN
+btnLoan.addEventListener('click', e => {
+  e.preventDefault();
+  console.log('Loaned');
+});
+
+// DELETE
+btnClose.addEventListener('click', e => {
+  e.preventDefault();
+  if (
+    +inputClosePin.value === currentAccount.pin &&
+    inputCloseUsername.value === currentAccount.username
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    containerApp.style.opacity = 0;
+    accounts.splice(index, 1);
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
+});
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -72,5 +214,7 @@ const currencies = new Map([
 ]);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
+const deposit = movements.filter(mov => mov > 0);
+const withdrawal = movements.filter(mov => mov < 0);
+const balance = movements.reduce((acc, cur) => acc + cur, 0);
 /////////////////////////////////////////////////
